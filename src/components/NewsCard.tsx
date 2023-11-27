db8b8ea320b25e125cbb0df4bf5c55586b570fef
Mahdi Fekhar <mahdi@zenown.com>
2023-11-27 19:05:29 +0400

    feat: add pagination to improuve performance

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,5 +1,7 @@
 // components/NewsCard.tsx
-import React from 'react';
+import React, { useState } from 'react';
+import { styled } from '@mui/system';
+import { Theme } from '@mui/system';
 
 interface NewsCardProps {
   title: string;
@@ -8,19 +10,71 @@ interface NewsCardProps {
   url: string;
 }
 
+const StyledNewsCard = styled('div')(({ theme }: { theme: Theme }) => ({
+    display: 'flex',
+    border: '1px solid',
+    borderColor: theme.palette.primary.main,
+    borderRadius: theme.shape.borderRadius,
+    overflow: 'hidden',
+    maxWidth: 800,
+    marginBottom: theme.spacing(2),
+  }));
+
+  const StyledImage = styled('img')(({ theme }: { theme: Theme }) => ({
+    minWidth: 450,
+    maxWidth: 450,
+
+    objectFit: 'cover',
+  }));
+  
+  const StyledContent = styled('div')(({ theme }: { theme: Theme }) => ({
+    padding: theme.spacing(2),
+  }));
+  
+  const StyledTitle = styled('h2')(({ theme }: { theme: Theme }) => ({
+    fontSize: '1.2rem',
+    marginBottom: theme.spacing(1),
+  }));
+  
+  const StyledDescription = styled('p')(({ theme }: { theme: Theme }) => ({
+    color: theme.palette.text.secondary,
+  }));
+  
+  const StyledLink = styled('a')(({ theme }: { theme: Theme }) => ({
+    textDecoration: 'none',
+    color: theme.palette.primary.main,
+    fontWeight: 'bold',
+  }));
 const NewsCard: React.FC<NewsCardProps> = ({ title, description, imageUrl, url }) => {
+    const [imageError, setImageError] = useState<boolean>(false);
+
+  const handleError = () => {
+    // Handle image loading errors
+    setImageError(true);
+  };
   return (
-    <div className="news-card">
-      <img src={imageUrl} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <a href={url} target="_blank" rel="noopener noreferrer">
+    <StyledNewsCard>
+      {!imageError ? (
+        <StyledImage
+          src={imageUrl}
+          alt={title}
+          onError={handleError}
+          style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
+        />
+      ) : (
+        <div className="image-error">Could not load image</div>
+      )}
+      <StyledContent>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDescription>{description}</StyledDescription>
+        <StyledLink href={url} target="_blank" rel="noopener noreferrer">
           Read more
-        </a>
-      </div>
-    </div>
+        </StyledLink>
+      </StyledContent>
+    </StyledNewsCard>
   );
 };
+
+
 
 export default NewsCard;
--- src/components/TabComponent.tsx
+++ src/components/TabComponent.tsx
@@ -1,12 +1,22 @@
 // components/TabComponent.tsx
 import React from 'react';
-import { Chip, Box } from '@mui/material';
+import { Chip, Box, styled } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 interface TabComponentProps {
   selectedTab: string;
   onTabChange: (tab: string) => void;
 }
+
+// Add your custom styles here
+const StyledChip = styled(Chip)(({ theme }) => ({
+  margin: theme.spacing(1),
+  cursor: 'pointer',
+  '&:hover': {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+  },
+}));
 
 const TabComponent: React.FC<TabComponentProps> = ({ selectedTab, onTabChange }) => {
   const { t } = useTranslation();
@@ -20,7 +30,7 @@ const TabComponent: React.FC<TabComponentProps> = ({ selectedTab, onTabChange })
   return (
     <Box display="flex" justifyContent="center" flexWrap="wrap" gap={1}>
       {tabs.map((tab) => (
-        <Chip
+        <StyledChip
           key={tab}
           label={t(tab)}
           onClick={() => handleTabChange(tab)}
